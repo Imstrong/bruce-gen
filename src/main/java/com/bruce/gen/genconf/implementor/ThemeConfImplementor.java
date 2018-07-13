5515165c87ab3ae1238971cd58dd19485b6adf1b
装逼之王 <zbzwcode@163.com>
2018-07-13 18:12:55 +0800

    继续引入桥接模式

--- src/main/java/com/bruce/gen/genconf/implementor/ThemeConfImplementor.java
+++ src/main/java/com/bruce/gen/genconf/implementor/ThemeConfImplementor.java
@@ -1,5 +1,17 @@
 package com.bruce.gen.genconf.implementor;
 
-public interface ThemeConfImplementor {
+import com.bruce.gen.genconf.vo.GenTypeModel;
 
+import java.util.Map;
+
+public interface ThemeConfImplementor {
+    /**
+     * 根据themeId和注册的theme配置参数获取theme中定义的能生成的功能类型
+     * @param themeId
+     * @param params
+     * @return
+     */
+    Map<String,GenTypeModel> getGenTypes(String themeId,Map<String,String> params);
+    Map<String,String> getGenOutTypes(String themeId,Map<String,String> params);
+    Map<String,String> getProviders(String themeId,Map<String,String> params);
 }
--- src/main/java/com/bruce/gen/genconf/manager/ConfManager.java
+++ src/main/java/com/bruce/gen/genconf/manager/ConfManager.java
@@ -1,33 +1,52 @@
 package com.bruce.gen.genconf.manager;
 
+import com.bruce.gen.genconf.implementor.GenConfImplementor;
 import com.bruce.gen.genconf.vo.GenConfModel;
 import com.bruce.gen.genconf.vo.ModuleConfModel;
+import com.bruce.gen.genconf.vo.NeedGenModel;
 
 import java.util.HashMap;
 import java.util.Map;
 
 //用于读取配置数据
 public class ConfManager {
-    private static ConfManager manager=new ConfManager();
-    private ConfManager(){
-
+    private static ConfManager manager=null;
+    private ConfManager(GenConfImplementor provider){
+        readConf(provider);
     }
-    public static ConfManager getInstance(){
+    public static ConfManager getInstance(GenConfImplementor provider){
+        if(manager==null){
+            return new ConfManager(provider);
+        }
         return manager;
     }
     //将配置数据缓存起来并保证只有一份实例
     private GenConfModel genConf=new GenConfModel();
     private Map<String,ModuleConfModel> moduleConfModelMap=new HashMap<>();
 
-    public GenConfModel getGenConf() {
-        return ConfManager.getInstance().getGenConf();
+    public GenConfModel getGenConf(GenConfImplementor provider) {
+        return this.genConf;
     }
 
     public Map<String, ModuleConfModel> getModuleConfModelMap() {
-        return ConfManager.getInstance().getModuleConfModelMap();
+        return this.moduleConfModelMap;
     }
 
-    private void readConf(){
-
+    private void readConf(GenConfImplementor provider){
+        //真正读取配置数据
+        readGenConf(provider);
+        for(NeedGenModel ngm:genConf.getNeedGens()){
+            readModuleGenConf(ngm);
+        }
+    }
+    private void readModuleGenConf(NeedGenModel ngm){
+        ModuleConfModel mcm=new ModuleConfModel();
+        mcm.setUseTheme(mcm.getUseTheme());
+        mcm.setExtendConfs();
+    }
+    private void readGenConf(GenConfImplementor provider){
+        genConf.setNeedGens(provider.getNeedGens());
+        genConf.setThemes(provider.getThemes());
+        genConf.setMapConstants(provider.getConstants());
     }
 }
